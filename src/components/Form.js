99eda8fa13236999d7e130c44953ed8aabb40786
Anjali Singh <105203288+Anjali0048@users.noreply.github.com>
2023-06-11 17:57:10 +0530

    commit all

--- src/components/Form.js
+++ src/components/Form.js
@@ -46,7 +46,7 @@ const Form = ({firstName,lastName,phoneNo,email,state,city,dob,gender,handleSubm
             <input 
               className="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white focus:border-gray-500" 
               id="grid-first-name" 
-              type="number" 
+              type="tel" 
               placeholder="9258476310"
               value={phoneNo}
               onChange={handleChange}
@@ -61,7 +61,7 @@ const Form = ({firstName,lastName,phoneNo,email,state,city,dob,gender,handleSubm
             <input 
               className="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white focus:border-gray-500" 
               id="grid-last-name" 
-              type="text" 
+              type="email" 
               placeholder="Joe@gmail.com"
               value={email}
               onChange={handleChange}
